b4534b96593c113c494173742f36f6c1eb22e5dc
Alex Coupe <acoupe@gmail.com>
2020-02-18 15:23:21 +0000

    Toggling state in a second context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { ThemeContext } from './../contexts/ThemeContext';
+import { LockedContext } from '../contexts/LockedContext';
 
 export default function Navbar() {
     return (
+        <LockedContext.Consumer>{(lockedContext) => (
         <ThemeContext.Consumer>
-            {(context) => {
-            const {isLightTheme, light, dark} = context.state;
+            {(themeContext) => {
+            const {isLocked, toggleLocked} = lockedContext;
+            const {isLightTheme, light, dark} = themeContext.state;
             const theme = isLightTheme ? light : dark;
             return (
         <div>
             <nav style={{background: theme.ui, color: theme.text }}>
                 <h1>Playlist App</h1>
+                <div>
+                    <button  onClick={toggleLocked}>{isLocked ? 'Playlist locked' : 'Playlist unlocked'}</button>
+                </div>
                 <ul>
                     <li>Home</li>
                     <li>About</li>
@@ -19,5 +25,6 @@ export default function Navbar() {
             </nav>
         </div>
         )}}</ThemeContext.Consumer>
+        )}</LockedContext.Consumer>
     )
 }
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { ThemeContext } from './../contexts/ThemeContext';
+import { LockedContext } from '../contexts/LockedContext';
 
 export default function ThemeToggle() {
     return (
-        <ThemeContext.Consumer>
-        {(context) => {
-            return (
-            <button onClick={context.toggleTheme}>Toggle Theme!</button>)
-        }}</ThemeContext.Consumer>
+        <LockedContext.Consumer>{(locked) => (
+            <ThemeContext.Consumer>
+            {(theme) => {
+                return (
+                <button onClick={theme.toggleTheme}>Toggle Theme!</button>)
+            }}</ThemeContext.Consumer>
+        )}</LockedContext.Consumer>
     )
 }
