eabad9c56c17badfe175a9058170344e11822599
Alex Coupe <acoupe@gmail.com>
2020-02-18 12:05:40 +0000

    Setting up base components

---
+++ src/components/Playlist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Playlist() {
+    return (
+        <div>
+            <h3>Madchester Playlist</h3>
+            <ul>
+                <li>Waterfall - The Stone Roses</li>
+                <li>Liver Forever - Oasis</li>
+                <li>Step On - The Happy Mondays</li>
+            </ul>
+        </div>
+    )
+}
